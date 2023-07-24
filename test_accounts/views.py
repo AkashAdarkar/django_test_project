6b0051d9299bddd1adb0d5c41695974dbc7ecb3b
akash <aregret01@gmail.com>
2023-07-24 23:21:31 +0530

    "lesson2 - URLS and views ."

--- test_accounts/views.py
+++ test_accounts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return HttpResponse("home Page")
+
+def about(request):
+    return HttpResponse("about")
+
+def products(request):
+    return HttpResponse("Products")
+
+
+def customers(request):
+    return HttpResponse("Customers")
